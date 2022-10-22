d543a8d6f2eb820ba2249743a6f9436f04125e26
Ejsi Veshaj <eveshaj@jacobs-university.de>
2022-10-22 14:56:32 +0200

    last_commit_assignment_4

--- main_assignment/index.js
+++ main_assignment/index.js
@@ -1,63 +1,84 @@
+const rock_button = document.getElementById("rock");
+const paper_button = document.getElementById("paper");
+const scissors_button = document.getElementById("scissors");
+
+let player_scores = 0;
+let computer_scores = 0;
+let round = 1;
+
 function computerPlay() {
   let options = ["Rock", "Paper", "Scissors"];
   return options[Math.floor(Math.random() * options.length)];
 }
 
 function playRound(playerSelection, computerSelection) {
+  const round_header = document.createElement("h3");
+  const round_result_p = document.createElement("p");
+  round_result_p.setAttribute("class", "round_result_p");
+  const players_results_p = document.createElement("p");
+
   let temp_string = playerSelection.toLowerCase();
   playerSelection =
     playerSelection.charAt(0).toUpperCase() + temp_string.slice(1);
+
+  let round_result;
+
   if (playerSelection === computerSelection) {
-    return "Draw.";
+    round_result = "Draw.";
   } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
-    return "You win! Rock beats Scissors.";
+    round_result = "You win! Rock beats Scissors.";
+    player_scores += 1;
   } else if (playerSelection === "Rock" && computerSelection === "Paper") {
-    return "You lose! Paper beats Rock.";
+    round_result = "You lose! Paper beats Rock.";
+    computer_scores += 1;
   } else if (playerSelection === "Paper" && computerSelection === "Scissors") {
-    return "You lose! Scissors beats Paper.";
+    round_result = "You lose! Scissors beats Paper.";
+    computer_scores += 1;
   } else if (playerSelection === "Paper" && computerSelection === "Rock") {
-    return "You win! Paper beats Rock.";
+    round_result = "You win! Paper beats Rock.";
+    player_scores += 1;
   } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
-    return "You win! Scissors beats Paper.";
+    round_result = "You win! Scissors beats Paper.";
+    player_scores += 1;
   } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
-    return "You lose! Rock beats Scissors.";
+    round_result = "You lose! Rock beats Scissors.";
+    computer_scores += 1;
   }
+
+  round_header.innerHTML = `Round: ${round}`;
+  round_result_p.innerHTML = round_result;
+  players_results_p.innerHTML = `<strong>Player scores:</strong> ${player_scores}  <b>|</b>   <strong>Computer scores:</strong> ${computer_scores}.`;
+
+  for (let x of [round_header, round_result_p, players_results_p]) {
+    document.getElementById("results").appendChild(x);
+  }
+
+  checkWins();
+
+  round += 1;
 }
 
-function game() {
-  let playerSelection;
-  let computerSelection;
-  let options = ["rock", "paper", "scissors"];
-  let playerScores = 0;
-  let computerScores = 0;
-
-  for (let i = 0; i < 5; i++) {
-    do {
-      playerSelection = prompt(
-        "Please write Rock, Scissors or Paper to start the round: "
-      );
-      tempPlayerSelection = playerSelection.toLowerCase();
-    } while (!options.includes(tempPlayerSelection));
-
-    computerSelection = computerPlay();
-
-    let result = playRound(playerSelection, computerSelection);
-    console.log(result);
-
-    if (result.includes("win")) {
-      playerScores += 1;
-    } else if (result.includes("lose")) {
-      computerScores += 1;
+function checkWins() {
+  if (player_scores === 5 || computer_scores === 5) {
+    let final_result;
+    if (player_scores > computer_scores) {
+      final_result = "You won.";
+    } else {
+      final_result = "Computer won.";
     }
-  }
-
-  if (playerScores === computerScores) {
-    console.log("Draw!");
-  } else if (playerScores < computerScores) {
-    console.log("You lost!");
-  } else {
-    console.log("You won!");
+    alert(`Game is finished. ${final_result}`);
+    location.reload();
   }
 }
 
-game();
+rock_button.addEventListener("click", function () {
+  playRound("rock", computerPlay());
+});
+
+paper_button.addEventListener("click", function () {
+  playRound("paper", computerPlay());
+});
+
+scissors_button.addEventListener("click", function () {
+  playRound("scissors", computerPlay());
+});
